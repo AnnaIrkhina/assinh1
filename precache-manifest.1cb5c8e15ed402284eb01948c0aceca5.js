affaf0f9014d38b82b19b05afa0f14b610036040
ponka123 <anka.irhina@gmail.com>
2020-06-12 14:40:43 -0700

    Updates

---
+++ precache-manifest.1cb5c8e15ed402284eb01948c0aceca5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e992c471f9c18bcfe1c0af0d86b760",
+    "url": "/AnnaIrkhina.github.io/assinh1/index.html"
+  },
+  {
+    "revision": "8fc9f34945dfe53ab557",
+    "url": "/AnnaIrkhina.github.io/assinh1/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9ee42ca032a75a2298c8",
+    "url": "/AnnaIrkhina.github.io/assinh1/static/css/main.41e0337f.chunk.css"
+  },
+  {
+    "revision": "8fc9f34945dfe53ab557",
+    "url": "/AnnaIrkhina.github.io/assinh1/static/js/2.a10d8bd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AnnaIrkhina.github.io/assinh1/static/js/2.a10d8bd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee42ca032a75a2298c8",
+    "url": "/AnnaIrkhina.github.io/assinh1/static/js/main.fc1f8354.chunk.js"
+  },
+  {
+    "revision": "2f4c56344c6ddfb3cfab",
+    "url": "/AnnaIrkhina.github.io/assinh1/static/js/runtime-main.07461ccb.js"
+  }
+]);
